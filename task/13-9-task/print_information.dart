00dff5b8d938b317bddae751e1fb122b6f463288
hamirdangar <109734153+hamirdangar@users.noreply.github.com>
2022-09-15 20:55:12 +0530

    Add files via upload

---
+++ task/13-9-task/print_information.dart
@@ -0,0 +1,8 @@
+
+
+void main(){
+  print("may name is hamir dangar");
+  print("birthe date is 2/12/2001");
+  print("age is 21 years old");
+  print("address is rajkot");
+}
---
+++ task/13-9-task/sum_of_5_sub.dart
@@ -0,0 +1,10 @@
+void main(){
+  int s1=88,s2=76,s3=65,s4=99,s5=77,sum;
+  double per;
+  print("sum of five sub :");
+  sum = s1+s2+s3+s4+s5;
+  print(sum);
+  print("percentage is :");
+  per = sum/5;
+  print(per);
+}
