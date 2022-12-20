9b8e3fe2f7bfe6afb25a8d91de983ee0f345a62b
hamirdangar <109734153+hamirdangar@users.noreply.github.com>
2022-12-20 13:47:58 +0530

    Update main.dart

--- assignment/module2/designing8/main.dart
+++ assignment/module2/designing8/main.dart
@@ -1,87 +1,58 @@
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget
-{
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+ 
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-     title: 'hamir',
-     home: HomePage(),
-   );
+    return MaterialApp(
+        home: Scaffold(
+           appBar: AppBar(
+             title: Text('Slider Seekbar in Flutter')
+             ),
+            body: Center(
+              child: SliderWidget()
+              )
+            )
+          );
   }
-
 }
-
-class HomePage extends StatefulWidget
-{
+ 
+class SliderWidget extends StatefulWidget {
   @override
-  State<HomePage> createState() => _HomePageState();
+  SiderWidgetState createState() => new SiderWidgetState();
 }
-
-class _HomePageState extends State<HomePage>
-{
-  var ans;
+ 
+class SiderWidgetState extends State{
+ 
+  int valueHolder = 20;
+      
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     appBar:  AppBar(
-       title: Text('appBar'),
-     ),
-     body: Container(
-       color: ans,
-       padding: EdgeInsets.all(15),
-       child: Center(
-         child: Column(
-           children: [
-             ListTile(
-               title: const Text('pink'),
-               leading: Radio(
-                 value: 2,
-                 groupValue: ans,
-                 activeColor: Colors.black,
-                 onChanged: (value)
-                 {
-                   setState(()
-                   {
-                      ans = Colors.red;
-                   });
-                 },
-               ),
-             ),
-             ListTile(
-               title: const Text('green'),
-               leading: Radio(
-                 value: 3,
-                 groupValue: ans,
-                 activeColor: Colors.black,
-                 onChanged: (value) {
-                   setState(() {
-                      ans = Colors.cyan;
-                   });
-                 },
-               ),
-             ),
-             ListTile(
-               title: const Text('blue'),
-               leading: Radio(
-                 value: 4,
-                 groupValue: ans,
-                 activeColor: Colors.black,
-                 onChanged: (value) {
-                   setState(() {
-                      ans = Colors.green;
-                   });
-                 },
-               ),
-             ),
-           ],
-         ),
-       ),
-     ),
-   );
+    return Center(
+      child: Column(
+      children: [
+        Container(
+          margin: EdgeInsets.fromLTRB(0, 100, 0, 0),
+          child: Slider(
+              value: valueHolder.toDouble(),
+              min: 1,
+              max: 100,
+              divisions: 100,
+              activeColor: Colors.green,
+              inactiveColor: Colors.grey,
+              label: '${valueHolder.round()}',
+              onChanged: (double newValue) {
+                setState(() {
+                  valueHolder = newValue.round();
+                });
+              },
+              semanticFormatterCallback: (double newValue) {
+                return '${newValue.round()}';
+              }
+          )),
+      Text('$valueHolder', style: TextStyle(fontSize: 22),)
+ 
+    ]
+  ));
   }
 }
