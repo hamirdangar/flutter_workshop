921f5cb34a5aa0aa9a465d30c762a3ebe7f6c025
hamirdangar <109734153+hamirdangar@users.noreply.github.com>
2023-02-23 12:02:17 +0530

    Add files via upload

--- assignment/module6/lib/main.dart
+++ assignment/module6/lib/main.dart
@@ -1,12 +1,22 @@
-//@dart=2.9
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'Screen/todo_list.dart';
 
-void main()
+import 'login.dart';
+
+void main() async
 {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
+    home: MyApp(),
     debugShowCheckedModeBanner: false,
-        home: TodoListPage(),
-    )
-  );
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return LoginScreen();
+  }
 }
